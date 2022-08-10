cc81bfcd2dfb72cd4519484ea40092ff446a961c
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-08-09 23:28:49 -0300

    correcao erros

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-     <Login />
+     {/* <Login /> */}
+     <Home />
     </div>
   );
 }
