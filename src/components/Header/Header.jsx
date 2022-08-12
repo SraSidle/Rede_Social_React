9c7a756c5efef933426a74b3322584b5db75c6cb
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-08-11 22:06:49 -0300

    posição itens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,14 @@ import "./Header.css";
 
 function Header(){
     return(
-        <div className="header">
+        <header>
             <h2 className="titulo-app">Orkut</h2>
             <nav>
                 <ul>Inicio</ul>
                 <ul>Perfil</ul>
                 <ul>Comunidade</ul>
             </nav>
-        </div>
+        </header>
     )
 }
 
